617a21a67f2611294d55bfad5fff9d826031b7fa
Anihs753 <akhobragade@kabbage.com>
2019-07-30 00:07:40 +0530

    changing port

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ import * as express from "express";
 import * as bodyParser from "body-parser"
 import { Db } from "./db"
 import { Routes } from "./route"
-const port = process.env.PORT || "3000";
+const port = process.env.PORT || "5000";
 
 class App {
 
